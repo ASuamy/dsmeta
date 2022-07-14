870239ff45385534a2e39c063d1cddf7c0ec2869
Asuamy <anandasuamy@gmail.com>
2022-07-13 21:07:24 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,5 +1,7 @@
 import './styles.css'
 import NotificationButton from '../NotificationButton'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function SalesCard() {
     return (
@@ -7,70 +9,80 @@ function SalesCard() {
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
-            </div>
 
-            <div>
-                <table className="dsmeta-sales-table">
-                    <thead>
-                        <tr>
-                            <th className="dsmeta-col-id">ID</th>
-                            <th className="dsmeta-col-data">Data</th>
-                            <th>Vendedor</th>
-                            <th className="dsmeta-col-visitas">Visitas</th>
-                            <th className="dsmeta-col-vendas">Vendas</th>
-                            <th>Total</th>
-                            <th>Notificar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="dsmeta-col-id">#123</td>
-                            <td className="dsmeta-col-data">12/07/22</td>
-                            <td>Anakin</td>
-                            <td className="dsmeta-col-visitas">11</td>
-                            <td className="dsmeta-col-vendas">35</td>
-                            <td>R$55300</td>
-                            <td>
-                                <div className="dsmeta-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="dsmeta-col-id">#123</td>
-                            <td className="dsmeta-col-data">12/07/22</td>
-                            <td>Anakin</td>
-                            <td className="dsmeta-col-visitas">11</td>
-                            <td className="dsmeta-col-vendas">35</td>
-                            <td>R$55300</td>
-                            <td>
-                                <div className="dsmeta-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="dsmeta-col-id">#123</td>
-                            <td className="dsmeta-col-data">12/07/22</td>
-                            <td>Anakin</td>
-                            <td className="dsmeta-col-visitas">11</td>
-                            <td className="dsmeta-col-vendas">35</td>
-                            <td>R$55300</td>
-                            <td>
-                                <div className="dsmeta-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                <div>
+                    <table className="dsmeta-sales-table">
+                        <thead>
+                            <tr>
+                                <th className="dsmeta-col-id">ID</th>
+                                <th className="dsmeta-col-data">Data</th>
+                                <th>Vendedor</th>
+                                <th className="dsmeta-col-visitas">Visitas</th>
+                                <th className="dsmeta-col-vendas">Vendas</th>
+                                <th>Total</th>
+                                <th>Notificar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="dsmeta-col-id">#123</td>
+                                <td className="dsmeta-col-data">12/07/22</td>
+                                <td>Anakin</td>
+                                <td className="dsmeta-col-visitas">11</td>
+                                <td className="dsmeta-col-vendas">35</td>
+                                <td>R$55300</td>
+                                <td>
+                                    <div className="dsmeta-btn-container">
+                                        <NotificationButton />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="dsmeta-col-id">#123</td>
+                                <td className="dsmeta-col-data">12/07/22</td>
+                                <td>Anakin</td>
+                                <td className="dsmeta-col-visitas">11</td>
+                                <td className="dsmeta-col-vendas">35</td>
+                                <td>R$55300</td>
+                                <td>
+                                    <div className="dsmeta-btn-container">
+                                        <NotificationButton />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="dsmeta-col-id">#123</td>
+                                <td className="dsmeta-col-data">12/07/22</td>
+                                <td>Anakin</td>
+                                <td className="dsmeta-col-visitas">11</td>
+                                <td className="dsmeta-col-vendas">35</td>
+                                <td>R$55300</td>
+                                <td>
+                                    <div className="dsmeta-btn-container">
+                                        <NotificationButton />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
+            </div>
         </div>
     )
 
